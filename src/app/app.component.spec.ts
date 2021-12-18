904e8833f6f1a8f31192284a5ac1eda7b1f6d556
javapedia.net <javatutorials2016@gmail.com>
2021-12-17 21:45:36 -0500

    init version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import {
+  ComponentFixture,
+  ComponentFixtureAutoDetect,
+  TestBed,
+} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
+  });
+
+  let fixtureElement: ComponentFixture<AppComponent>;
+  let componentELement;
+  beforeEach(() => {
+    fixtureElement = TestBed.createComponent(AppComponent);
+    componentELement = fixtureElement.componentInstance;
   });
 
   it('should create the app', () => {
@@ -26,10 +34,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('tdd2-angular');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tdd2-angular app is running!');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain('tdd2-angular app is running!');
+  // });
 });
---
+++ src/app/header/header.component.spec.ts
@@ -0,0 +1,41 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show logo', () => {
+    expect(
+      fixture.nativeElement.querySelector('[data-test="logo"]')
+    ).toBeTruthy();
+  });
+
+  it('should show search', () => {
+    expect(
+      fixture.nativeElement.querySelector('[data-test="search"]')
+    ).toBeTruthy();
+  });
+  it('should show menu', () => {
+    expect(
+      fixture.nativeElement.querySelector('[data-test="menu"]')
+    ).toBeTruthy();
+  });
+});
